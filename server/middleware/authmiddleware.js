5c9e0a7bd049138ea278b343ce4fcc749dbc329a
akash chauhan <sandeepakash537@gmail.com>
2025-06-21 23:01:16 +0530

    Added authmiddleware.js with correct casing

--- server/middleware/authmiddleware.js
+++ server/middleware/authmiddleware.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const authmiddleware = (req,res,next)=>{
-    const authHeader  = req.headers.authorization;
+const authmiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(401).json({message:"No token provided"})
-    }
-    
-    const token = authHeader.split(' ')[1];
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
 
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    }
-    catch(err){
-        res.status(403).json({message:"Invalid or expired token"});
-    }
-}
-module.exports = authmiddleware
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: 'Invalid or expired token' });
+  }
+};
+
+module.exports = authmiddleware;
